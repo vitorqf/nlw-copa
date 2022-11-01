0c5ad100eb207653d1c19c29201fe846377cce6d
Vitor Rafael <rqfvitor@gmail.com>
2022-11-01 20:22:57 -0300

    feat: mobile setup done

---
+++ mobile/src/screens/SignIn.tsx
@@ -0,0 +1,8 @@
+import { Center, Text } from 'native-base';
+export function SignIn() {
+    return (
+        <Center flex={1} bgColor="gray.900">
+            <Text color="gray.200" fontSize={24}>Hello World</Text>
+        </Center>
+    )
+}
