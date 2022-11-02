d9a9dc8026d97e250f6826350c433ebcb19c9ebf
Vitor Rafael <rqfvitor@gmail.com>
2022-11-02 12:41:17 -0300

    feat: landing page

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -3,74 +3,99 @@ import appMockup from '../assets/app-mockup.png'
 import logoImg from '../assets/logo.svg'
 import usersAvatarExampleImg from '../assets/users-avatar-example.png'
 import iconCheck from '../assets/check.svg'
+import { api } from '../lib/axios'
 
 interface HomeProps {
-  count: number
+  poolCount: number
+  guessCount: number
+  userCount: number
 }
 
 export default function Home(props: HomeProps) {
   return (
-    <div>
+    <div className='max-w-[1124px] h-screen mx-auto grid grid-cols-2 gap-28 items-center'>
       <main>
         <Image src={logoImg} alt="NLW Copa" />
 
-        <h1>Crie seu próprio bolão da copa e compartilhe entre amigos!</h1>
+        <h1 
+          className='mt-14 text-white text-5xl font-bold leading-tight'
+        >
+          Create your own pool and share with your friends!
+        </h1>
 
-        <div>
+        <div
+          className='mt-10 flex items-center gap-2'        
+        >
           <Image src={usersAvatarExampleImg} alt=""/>
-          <strong>
-            <span>+12.592</span> pessoas já usando
+          <strong className='text-gray-100 text-xl'>
+            <span className='text-ignite-500'>+{props.userCount}</span> already using!
           </strong>
         </div>
 
-        <form>
-          <input type="text" required placeholder='Qual nome do seu bolão?' />
-          <button type="submit">Criar meu bolão</button>
+        <form className='mt-10 flex gap-2'>
+          <input 
+            type="text" 
+            required 
+            placeholder="What's your pool name?"
+            className='flex-1 px-6 py-4 rounded bg-gray-800 border border-gray-600 text-sm' 
+          />
+          <button 
+            type="submit"
+            className='bg-yellow-500 px-6 py-4 rounded uppercase text-gray-900 font-bold text-sm hover:bg-yellow-700 active:bg-yellow-700 transition-colors'
+          >Create pool</button>
         </form>
 
-        <p>
-          Após criar seu bolão, você receberá um código único que poderá usar para convidar outras pessoas 🚀
+        <p className='mt-4 text-sm text-gray-300 leading-relaxed'>
+          After creating your pool, you will receive an unique code to share 🚀
         </p>
 
-        <div>
+        <div className='mt-10 pt-10 border-t border-gray-600 flex items-center justify-between text-gray-100'>
 
-          <div>
+          <div className='flex items-center gap-6'>
             <Image src={iconCheck} alt="" />
-            <div>
-              <span>+2.034</span>
-              <span>Bolões criados</span>
+            <div className='flex flex-col'>
+              <span className='text-2xl font-bold'>+{props.poolCount}</span>
+              <span>Created pools</span>
             </div>
           </div>
 
-          <div>
+          <div className='w-px h-14 bg-gray-600'/>
+
+          <div className='flex items-center gap-6'>
             <Image src={iconCheck} alt="" />
-            <div>
-              <span>+2.034</span>
-              <span>Bolões criados</span>
+            <div className='flex flex-col'>
+              <span className='text-2xl font-bold'>+{props.guessCount}</span>
+              <span>Sent guesses</span>
             </div>
           </div>
 
         </div>
-        
+
       </main>
 
       <Image 
+        className='animate-floating translate-y-0 transition-all'
         src={appMockup} 
-        alt="Dois celulares exibindo uma prévia da aplicação móvel do NLW Copa"
+        alt="Two mobile devices showing a preview of NLW-Copa application"
+        quality={100}
       />
+
     </div>
   )
 }
 
 export const getServerSideProps = async () => {
-  const response = await fetch('http://localhost:3333/pools/count')
-  const data = await response.json()
-
-  console.log(data)
+  const [poolCountResponse, guessCountResponse, userCountResponse] = await Promise.all([
+    api.get('/pools/count'),
+    api.get('/guesses/count'),
+    api.get('/users/count')
+  ])
 
   return {
     props: {
-      count: data.count,
+      poolCount: poolCountResponse.data.count,
+      guessCount: guessCountResponse.data.count,
+      userCount: userCountResponse.data.count,
     }
   }
 }
--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -9,8 +9,45 @@ module.exports = {
         sans: 'Roboto, sans-serif'
       },
 
+      backgroundImage: {
+        app: 'url(/background.png)'
+      },
+
+      keyframes: {
+        floating: {
+          '0%': {
+            transform: 'translatey(0px)'
+          },
+          '50%': {
+            transform: 'translatey(-15px)'
+          },
+          '100%': {
+            transform: 'translatey(0px)'
+          },
+        }
+      },
+
+      animation: {
+        floating: 'floating 5s ease-in-out infinite'
+      },
+
       colors: {
-        900: '#121214',
+        gray: {
+          100: '#E1E1E6',
+          300: '#8D8D99',
+          600: '#323238',
+          800: '#202024',
+          900: '#121214'
+        },
+
+        ignite: {
+          500: '#129E57'
+        },
+
+        yellow: {
+          500: '#F7DD43',
+          700: '#E5CD3D'
+        }
       }
     },
   },
