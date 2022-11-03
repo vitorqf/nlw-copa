e8dda16917e30736adc28482a2c838b13060ed06
Vitor Rafael <rqfvitor@gmail.com>
2022-11-03 18:43:55 -0300

    feat: signin page layout

--- mobile/src/screens/SignIn.tsx
+++ mobile/src/screens/SignIn.tsx
@@ -1,8 +1,25 @@
-import { Center, Text } from 'native-base';
+import { Center, Icon, Text } from 'native-base';
+import { Fontisto } from '@expo/vector-icons'
+
+import Logo from '../assets/logo.svg'
+import { Button } from '../components/Button';
+
 export function SignIn() {
     return (
-        <Center flex={1} bgColor="gray.900">
-            <Text color="gray.200" fontSize={24}>Hello World</Text>
+        <Center flex={1} bgColor="gray.900" p={7}>
+            <Logo width={212} height={40}/>
+
+            <Button 
+                title="LOGIN WITH GOOGLE"
+                type="SECONDARY"
+                leftIcon={<Icon as={Fontisto} name="google" color="white" size="md"/>}
+                mt={12}
+            />
+
+            <Text color="gray.200" textAlign="center" mt={4}>
+                We won't use any information beside {'\n'}
+                your e-mail for account creating.
+            </Text>
         </Center>
     )
 }
